8caeeab424515a6ca853302d6472bc5ae9f767f2
Isaiah Campsall <isaiahcampsall@gmail.com>
2023-10-19 14:56:46 -0400

    adding dynamics to html page

--- client/index.js
+++ client/index.js
@@ -1,22 +1,44 @@
 
+const postContent = `
+<form id="new-envelope" action="http://localhost:3000/envelopes/" method="POST">
+                
+    <label for="name">Name</label>
+    <input type="text" name="name" required> <br>
 
-let testButton = document.querySelector('#click');
+    <label for="balance">Balance</label>
+    <input type="number" name="balance" required> <br>
+
+    <label for="submit-new-envelope">NEW Envelope</label>
+    <input type="submit" name="submit-new-envelope"> <br>
+
+</form>
+`
+
+let homeButton = document.querySelector('#home');
+let buttonsBar = document.querySelector('#buttons');
+let content = document.querySelector('#dynamic');
+let postButton = document.querySelector('#post');
 let testMessage = document.querySelector('#test-message');
+let newEnvelopeForm = document.querySelector('#new-envelope');
 
 const getTestData = async () => {
     let response = await fetch("http://localhost:3000/envelopes/");
     const data = await response.json();
     return data;
-    
 }
 
-testButton.onclick = () => {
-    getTestData().then(data => {
-        let string = '';
-        data.forEach((element, index) => {
-            string += data[index].name + '<br>';
-        });
-        testMessage.innerHTML = string;
-        console.log(data[0].name);
-    })
+postButton.onclick = () => {
+    content.innerHTML = postContent;
+    buttonsBar.setAttribute('style', 'display: none');
+    homeButton.setAttribute('style', 'display: block');
+}
+
+homeButton.onclick = () => {
+    content.innerHTML = '';
+    homeButton.setAttribute('style', 'display: none');
+    buttonsBar.setAttribute('style', 'display: block');
+}
+
+newEnvelopeForm.onsubmit = () => {
+
 }
